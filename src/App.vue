cdc63918b3ce09063e6e30232504018c3b030733
ydfs <3219747974@qq.com>
2021-04-26 10:10:39 +0800

    my first submit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,129 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <section class="todoapp" v-cloak>
+      <the-header class="header" @add-todo="handleAddTodo">
+        <!-- <h1>todos</h1>
+        <input class="new-todo" autofocus autocomplete="off" placeholder="What needs to be done?" v-model="newTodo" @keyup.enter="handleAddTodo"> -->
+      </the-header>
+      <the-list :todos.sync="todos" :filter="filter"></the-list>
+      <!-- <section class="main">
+        <input id="toggle-all" class="toggle-all" type="checkbox" v-model="allCompleted">
+        <label for="toggle-all">Mark all as complete</label>
+        <ul class="todo-list">
+          <li :class="['todo', {completed: todo.completed},{editing: editTodo === todo}]" v-for="(todo,index) in showTodos" :key="index">
+            <div class="view">
+              <input class="toggle" type="checkbox" v-model="todo.completed">
+              <label @dblclick="handleEdit(todo)">{{todo.title}}</label>
+              <button class="destroy" @click="handleRemoveTodo(todo)"></button>
+            </div>
+            <input class="edit" type="text" v-model="todo.title" v-todo-focus="editTodo === todo" @blur="handleEditDone(todo)" @keyup.enter="handleEditDone(todo)" @keyup.esc="handleEditCancel(todo)">
+          </li>
+        </ul>
+      </section> -->
+      <footer class="footer">
+        <span class="todo-count">
+          <strong>3</strong> todo
+        </span>
+        <ul class="filters">
+          <li><a href="#/all" :class="{selected: filter === 'all'}" @click="handleChangeFilter('all')">All</a></li>
+          <li><a href="#/active" :class="{selected: filter === 'active'}" @click="handleChangeFilter('active')">Active</a></li>
+          <li><a href="#/completed" :class="{selected: filter === 'completed'}" @click="handleChangeFilter('completed')">Completed</a></li>
+        </ul>
+        <button class="clear-completed" v-show="hasCompleted" @click="handleRemoveAllCompletedTodo">Clear completed</button>
+      </footer>
+    </section>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import '@/assets/todos-html/base.css'
+import '@/assets/todos-html/index.css'
+import TheHeader from '@/components/TheHeader'
+import TheList from '@/components/TheList'
+export default {
+  data () {
+    return {
+      todos: [{
+        title: '任务 一',
+        completed: false
+      }, {
+        title: '任务 二',
+        completed: true
+      }, {
+        title: '任务 三',
+        completed: true
+      }],
+      editTodo: null,
+      beforeEditCache: '',
+      newTodo: null,
+      filter: 'all'
     }
+  },
+  methods: {
+    handleEdit (todo) {
+      this.editTodo = todo
+      this.beforeEditCache = todo.title
+    },
+    handleEditDone (todo) {
+      if (todo.title.trim()) {
+        this.editTodo = null
+        this.beforeEditCache = null
+      }
+    },
+    handleEditCancel (todo) {
+      todo.title = this.beforeEditCache
+      this.editTodo = null
+      this.beforeEditCache = null
+    },
+    handleAddTodo (todo) {
+      this.todos.push(todo)
+    },
+    handleRemoveTodo (todo) {
+      const index = this.todos.indexOf(todo)
+      this.todos.splice(index, 1)
+    },
+    handleRemoveAllCompletedTodo () {
+      this.todos = this.todos.filter(data => !data.completed)
+    },
+    handleChangeFilter (filter) {
+      this.filter = filter
+    }
+  },
+  directives: {
+    'todo-focus': function (el, binding) {
+      if (binding.value) {
+        el.focus()
+      }
+    }
+  },
+  computed: {
+    allCompleted: {
+      get: function () {
+        return this.todos.every(data => data.completed)
+      },
+      set: function (value) {
+        this.todos.forEach(todo => { todo.completed = value })
+      }
+    },
+    hasCompleted () {
+      return this.todos.some(data => data.completed)
+    },
+    showTodos () {
+      const filter = this.filter
+      return this.todos.filter(data => {
+        if (filter === 'all') {
+          return true
+        } else if (filter === 'active') {
+          return !data.completed
+        } else if (filter === 'completed') {
+          return data.completed
+        }
+      })
+    }
+  },
+  components: {
+    'the-header': TheHeader,
+    'the-list': TheList
   }
 }
-</style>
+</script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="header">
+    <h1>todos</h1>
+    <input class="new-todo" autofocus autocomplete="off" placeholder="需要做什么呢 ？" v-model="newTodo" @keyup.enter="addTodo">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+  props: {
+    todos: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data () {
+    return {
+      newTodo: ''
+    }
+  },
+  methods: {
+    addTodo: function () {
+      this.$emit('add-todo', {
+        title: this.newTodo,
+        completed: false
+      })
+      this.newTodo = ''
+    }
+  }
+}
+</script>
